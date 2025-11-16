329cdce258e772d660d8e7ad0ecef4e71520db36
gwiyeoni <bcwqcd@gmail.com>
2025-11-16 16:30:45 +0900

    first commit

---
+++ src/main/java/com/copyleft/GodsChoice/GodsChoiceApplication.java
@@ -0,0 +1,13 @@
+package com.copyleft.GodsChoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodsChoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodsChoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/copyleft/GodsChoice/GodsChoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.copyleft.GodsChoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodsChoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
