eda6736e63b7d35fb71cfb9e65e4449a5d7ce3e5
gwiyomi <bcwqcd@gmail.com>
2025-12-02 17:31:11 +0900

    feat : 채팅 DTO 정의

---
+++ src/main/java/com/copyleft/GodsChoice/feature/chat/dto/ChatRequest.java
@@ -0,0 +1,10 @@
+package com.copyleft.GodsChoice.feature.chat.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ChatRequest {
+    private String message;
+}
